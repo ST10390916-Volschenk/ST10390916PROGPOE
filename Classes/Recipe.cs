bfcf9d65e9a8c7675f70b7dd6f61b8fec3abb026
WeylinV <st10390916@vcconnect.edu.za>
2024-04-15 08:18:33 +0200

    Added references and comments

--- Classes/Recipe.cs
+++ Classes/Recipe.cs
@@ -1,4 +1,11 @@
-﻿using System;
+﻿// Weylin Volschenk
+// ST10390916
+// Group 1
+
+// References: 
+//             https://www.w3schools.com/cs/cs_syntax.php
+
+using System;
 using System.Collections.Generic;
 
 namespace ST10390916PROGPOE
@@ -47,3 +54,5 @@ namespace ST10390916PROGPOE
         }
     }
 }
+//------------------------------------------...ooo000 END OF FILE 000ooo...------------------------------------------------------//
+
